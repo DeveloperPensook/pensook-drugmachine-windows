9439d9855654a63675b70b193367c6eafb5c4bbf
DeveloperPensook <140368772+DeveloperPensook@users.noreply.github.com>
2023-10-10 13:44:18 +0700

    Update stockLedgerServices.js

--- services/stockLedgerServices.js
+++ services/stockLedgerServices.js
@@ -1,30 +1,22 @@
-const express = require("express");
-const ModbusRTU = require("modbus-serial");
-
 async function drugMachineModbus(req) {
     const { ip, port: modbusPort, slaveId, address, value, entryType, entryStatusAddress, doorStatusAddress } = req.body;
+    const client = new ModbusRTU();
 
     try {
-        const client = new ModbusRTU();
         await client.connectTCP(ip, { port: modbusPort });
-
-        const resMessage = [];
-        let isPassed = true;
 
         for (let i = 0; i < address.length; i++) {
             try {
                 await client.writeRegisters(address[i], [value[i]]);
-                resMessage.push(`Register ${address[i]} written successfully.`);
             } catch (err) {
                 console.error(`Error writing register ${address[i]}:`, err);
-                isPassed = false;
             }
         }
-
+    } catch (error) {
+        return { success: false, error: error.message };
+    } finally {
         await client.close();
-
-        // after writeRegisters read status of register
-        const length = 1; // Assuming you want to read one holding register
+        const length = 1;
         const getStatusAddress = entryType === 'Pickup Medicine' ? entryStatusAddress : doorStatusAddress;
         const expectResult = entryType === 'Pickup Medicine' ? 1 : 0;
 
@@ -34,7 +26,6 @@ async function drugMachineModbus(req) {
 
         let result;
 
-        // Define a function to read holding registers with a timeout
         async function readHoldingRegistersWithTimeout() {
             try {
                 const data = await client2.readHoldingRegisters(getStatusAddress, length);
@@ -42,10 +33,9 @@ async function drugMachineModbus(req) {
                     result = data.data[0];
                 }
                 if (result === expectResult) {
-                    let returnData = await updateToZero(ip, modbusPort, slaveId, [getStatusAddress], [0])
-                    return returnData
+                    let returnData = await updateToZero(ip, modbusPort, getStatusAddress, 0);
+                    return returnData;
                 } else {
-                    // If result is not as expected, force a repeated read every 1 second for up to 10 seconds
                     if (Date.now() - startTime < 60000) {
                         setTimeout(readHoldingRegistersWithTimeout, 1000);
                     } else {
@@ -53,43 +43,28 @@ async function drugMachineModbus(req) {
                     }
                 }
             } catch (error) {
-                throw error;
+                return { success: false, error: error.message };
             }
         }
 
         const startTime = Date.now();
         return (await readHoldingRegistersWithTimeout());
-    } catch (error) {
-        return { success: false, error: error.message };
-    }
+    } 
 }
 
-async function updateToZero(ip, modbusPort, slaveId, address, value) {
+async function updateToZero(ip, modbusPort, address, value) {
+    const client = new ModbusRTU();
     try {
-        const client = new ModbusRTU();
         await client.connectTCP(ip, { port: modbusPort });
-
-        const resMessage = [];
-        let isPassed = true;
-
-        for (let i = 0; i < address.length; i++) {
-            try {
-                await client.writeRegisters(address[i], [value[i]]);
-                resMessage.push(`Register ${address[i]} written successfully.`);
-                return {success: true}
-            } catch (err) {
-                console.error(`Error writing register ${address[i]}:`, err);
-                isPassed = false;
-            }
-        }
-
+        await client.writeRegisters(address, [value]);
         await client.close();
         return {success: true}
     } catch (error) {
         return { success: false, error: error.message };
+    } finally {
+        return {success: true}
     }
 }
-
 
 module.exports = {
     drugMachineModbus
