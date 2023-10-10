13273e47b0e02abd3d4afbd3c7d7cfda0e215208
DeveloperPensook <140368772+DeveloperPensook@users.noreply.github.com>
2023-10-10 11:57:58 +0700

    Update stockLedgerServices.js

--- services/stockLedgerServices.js
+++ services/stockLedgerServices.js
@@ -42,7 +42,9 @@ async function drugMachineModbus(req) {
                     result = data.data[0];
                 }
                 if (result === expectResult) {
+                    await updateToZero(ip, modbusPort, slaveId, [getStatusAddress], [0])
                     return { success: true };
+                    
                 } else {
                     // If result is not as expected, force a repeated read every 1 second for up to 10 seconds
                     if (Date.now() - startTime < 60000) {
@@ -63,67 +65,31 @@ async function drugMachineModbus(req) {
     }
 }
 
-async function getStatusModbus(req) {
-    const { ip, modbusPort, slaveId, address, length, entryType, entryStatusAddress, doorStatusAddress } = req.body;
-
+async function updateToZero(ip, modbusPort, slaveId, address, value) {
     try {
         const client = new ModbusRTU();
-        let startTime = Date.now(); // Record the start time
-
         await client.connectTCP(ip, { port: modbusPort });
-        await client.setID(slaveId);
 
-        let data;
-        let readComplete = false;
+        const resMessage = [];
+        let isPassed = true;
 
-        // Set a 5-minute (300000 milliseconds) timeout
-        const timeoutPromise = new Promise((_, reject) => {
-            setTimeout(() => {
-                reject(new Error("Timeout exceeded"));
-            }, 10000);
-        });
-
-        // Log elapsed time every second
-        const interval = setInterval(() => {
-            const elapsedTime = Math.floor((Date.now() - startTime) / 1000);
-            console.log(`Elapsed time: ${elapsedTime} seconds`);
-        }, 1000);
-
-        while (!readComplete) {
-            data = await Promise.race([
-                client.readHoldingRegisters(address, length),
-                timeoutPromise // Race with the timeout promise
-            ]);
-
-            if (data instanceof Error) {
-                throw data; // Propagate the error if it's an error
-            }
-
-            console.log("Holding registers:", data.data);
-
-            if (entryType == 'Pickup Medicine') {
-                if (data.data[0] === 1) {
-                    drugMachineModbus(ip, modbusPort, slaveId, [entryStatusAddress], [0]);
-                    readComplete = true;
-                } else {
-                    await new Promise(resolve => setTimeout(resolve, 1000));
-                }
-            } else {
-                if (data.data[0] === 0) {
-                    readComplete = true;
-                }
+        for (let i = 0; i < address.length; i++) {
+            try {
+                await client.writeRegisters(address[i], [value[i]]);
+                resMessage.push(`Register ${address[i]} written successfully.`);
+            } catch (err) {
+                console.error(`Error writing register ${address[i]}:`, err);
+                isPassed = false;
             }
         }
 
-        clearInterval(interval); // Stop logging elapsed time
         await client.close();
-        return { success: true };
     } catch (error) {
         return { success: false, error: error.message };
     }
 }
 
+
 module.exports = {
-    drugMachineModbus,
-    getStatusModbus
+    drugMachineModbus
 };
