919d20c42614140d325192deb4022f4ae596e55d
DeveloperPensook <140368772+DeveloperPensook@users.noreply.github.com>
2023-10-10 14:18:59 +0700

    Update stockLedgerServices.js

--- services/stockLedgerServices.js
+++ services/stockLedgerServices.js
@@ -3,70 +3,53 @@ const ModbusRTU = require("modbus-serial");
 async function drugMachineModbus(req) {
     const { ip, port: modbusPort, slaveId, address, value, entryType, entryStatusAddress, doorStatusAddress } = req.body;
     const client = new ModbusRTU();
-
-    try {
-        await client.connectTCP(ip, { port: modbusPort });
-
-        for (let i = 0; i < address.length; i++) {
-            try {
-                await client.writeRegisters(address[i], [value[i]]);
-            } catch (err) {
-                console.error(`Error writing register ${address[i]}:`, err);
-                isPassed = false;
-            }
+    await client.connectTCP(ip, { port: modbusPort });
+    for (let i = 0; i < address.length; i++) {
+        try {
+            await client.writeRegisters(address[i], [value[i]]);
+        } catch (err) {
+            return { success: false };
         }
+    }
+    const length = 1;
+    const getStatusAddress = entryType === 'Pickup Medicine' ? entryStatusAddress : doorStatusAddress;
+    const expectResult = entryType === 'Pickup Medicine' ? 1 : 0;
 
-        const length = 1;
-        const getStatusAddress = entryType === 'Pickup Medicine' ? entryStatusAddress : doorStatusAddress;
-        const expectResult = entryType === 'Pickup Medicine' ? 1 : 0;
+    const client2 = new ModbusRTU();
+    await client2.connectTCP(ip, { port: modbusPort });
+    await client2.setID(slaveId);
 
-        const client2 = new ModbusRTU();
-        await client2.connectTCP(ip, { port: modbusPort });
-        await client2.setID(slaveId);
+    let result;
 
-        let result;
-
-        async function readHoldingRegistersWithTimeout() {
-            try {
-                const data = await client2.readHoldingRegisters(getStatusAddress, length);
-                if (data.data[0]) {
-                    result = data.data[0];
+    async function readHoldingRegistersWithTimeout() {
+        try {
+            const data = await client2.readHoldingRegisters(getStatusAddress, length);
+            if (data.data[0]) {
+                result = data.data[0];
+            }
+            if (result === expectResult) {
+                const client3 = new ModbusRTU();
+                await client3.connectTCP(ip, { port: modbusPort });
+                try {
+                    await client3.writeRegisters(getStatusAddress, [0]);
+                    return { success: true };
+                } catch (err) {
+                    return { success: false };
                 }
-                if (result === expectResult) {
-                    let returnData = await updateToZero(ip, modbusPort, getStatusAddress, 0);
-                    return returnData;
+            } else {
+                if (Date.now() - startTime < 60000) {
+                    setTimeout(readHoldingRegistersWithTimeout, 1000);
                 } else {
-                    if (Date.now() - startTime < 60000) {
-                        setTimeout(readHoldingRegistersWithTimeout, 1000);
-                    } else {
-                        throw new Error("Timeout");
-                    }
+                    throw new Error("Timeout");
                 }
-            } catch (error) {
-                throw error;
             }
+        } catch (error) {
+            throw error;
         }
-
-        const startTime = Date.now();
-        return await readHoldingRegistersWithTimeout();
-    } catch (error) {
-        return { success: false, error: error.message };
-    } finally {
-        await client.close();
     }
-}
 
-async function updateToZero(ip, modbusPort, address, value) {
-    const client = new ModbusRTU();
-    try {
-        await client.connectTCP(ip, { port: modbusPort });
-        await client.writeRegisters(address, [value]);
-        return { success: true };
-    } catch (error) {
-        return { success: false, error: error.message };
-    } finally {
-        await client.close();
-    }
+    const startTime = Date.now();
+    return await readHoldingRegistersWithTimeout();
 }
 
 module.exports = {
